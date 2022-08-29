b9ff0deb8049fc4514d95c5e20251b8b03dd4578
unknown <denoche@gmail.com>
2022-08-29 13:07:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/e_commerceapp/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.e_commerceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+//ViewGroup - is an invisible container of other views
+// (child views) and other ViewGroup.
+
+//View - is a basic building block of UI
+// (User Interface) in android.
